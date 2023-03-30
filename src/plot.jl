1b4b83518182ec494e242dcaa4fb87d0d170dfb4
Daniel Bulanda <daniel@bulanda.net>
2023-03-30 20:23:48 +0200

    implement cube neurons template

--- src/plot.jl
+++ src/plot.jl
@@ -1,49 +1,177 @@
 export anakgplot
 
 using GLMakie
+using Random
+using Distributions
+using Colors
+using LinearAlgebra
 
 using AssociativeScenesSimulation
 
+mutable struct State
+    neuronpositions::Dict{UInt, Point3f}
+
+    State() = new(Dict())
+end
+
+state = State()
+
 function anakgplot(
     file::String=ANAKG_SAMPLE_MAT_FILE;
-    resolution=primary_resolution(), camera3d=true
+    resolution=primary_resolution(), camera3d=true, background=:azure1,
+    randomscale=0.3,
+    neuron_outercolor=RGBA{Float32}(0.53, 0.81, 1.0, 0.5), 
+    neuron_innercolor=RGBA{Float32}(0.0, 0.0, 0.55, 0.88),
+    neuron_outersize=0.2 , neuron_innersize=0.07
 )
+    global state = State()
+
     set_theme!(theme_black(), resolution=resolution)
     GLMakie.activate!(; framerate=120, fullscreen=true)
 
-    figure, parentscene, camera = createscenes(resolution, camera3d)
+    figure, parentscene, camera = createscenes(
+        resolution, camera3d, background
+    )
     
+    sides = 6
     data = anakgload(file)
     neuronsnumber = size(data, 1)
-    sidelength = ceil(√(neuronsnumber / 6))
-    siderange = LinRange(0.0, sidelength, Int(sidelength))
-
-    xs = Float64[]
-    ys = Float64[]
-    zs = Float64[]
-    for x=siderange, y=siderange
-        push!(xs, x)
-        push!(ys, y)
-        push!(zs, 0)
+    neuronsnumber_perside = neuronsnumber / sides
+    sidelength = √(neuronsnumber_perside)
+    sidelength_int = Int(floor(sidelength))
+    println("sidelength_int $sidelength_int")
+    lengthdiff = neuronsnumber - ceil(sidelength_int^2 * sides)
+    println("lengthdiff $lengthdiff")
+    
+    lengthdiff_int = lengthdiff ÷ (sidelength_int * sides)
+    println("lengthdiff_int $lengthdiff_int")
+    lengthdiff_rem = lengthdiff % sidelength_int
+    println("lengthdiff_rem $lengthdiff_rem")
+    sideranges = []
+    for i in 1:2sides
+        siderange = if i % 2 == 0
+            siderange = LinRange(
+                0.0, 
+                sidelength_int, 
+                sidelength_int + lengthdiff_int
+            )
+            siderange
+        else
+            if lengthdiff_rem > 0 && i == 2sides - 1
+                LinRange(
+                    0.0, 
+                    sidelength_int, 
+                    sidelength_int + 1
+                )
+            else
+                LinRange(0.0, sidelength_int, sidelength_int)
+            end
+        end
+        push!(sideranges, siderange)
     end
 
-    println("typeof(xs), $(typeof(xs))")
+    # siderangex, siderangey, siderangez, lengthdiff_rem = if lengthdiff > 3sides
+    #     lengthdiff_int = lengthdiff ÷ 3sides
+    #     range = LinRange(
+    #         0.0, 
+    #         sidelength_int + lengthdiff_int, 
+    #         sidelength_int + lengthdiff_int
+    #     )
+    #     range, range, range, lengthdiff % 3sides
+    # else
+    #     lengthdiff_int = lengthdiff ÷ sides 
+    #     range = LinRange(0.0, sidelength_int, sidelength_int)
+    #     zrange = LinRange(
+    #         0.0, 
+    #         sidelength_int + lengthdiff_int, 
+    #         sidelength_int + lengthdiff_int
+    #     )
+    #     range, range, zrange, lengthdiff % 3sides
+    # end
+    # siderangex = LinRange(0.0, sidelength_int, sidelength_int)
+    # siderangey = LinRange(0.0, sidelength_int, sidelength_int)
+    # siderangez = LinRange(0.0, sidelength_int, sidelength_int)
 
-    # xs = cos.(1:0.5:20)
-    # ys = sin.(1:0.5:20)
-    # zs = LinRange(0, 3, length(xs))
+    sideoffset = 0.05sidelength_int
+    lastside = 2sideoffset + sidelength_int
 
-    meshscatter!(parentscene, xs, ys, zs, markersize = 0.1, color = zs)
+    neuronid = 1
+    for i in 1:sides
+        if i == 1
+            for x=sideranges[1], y=sideranges[2]
+                xpos = x + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                ypos = y + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(xpos, ypos, 0)
+                neuronid += 1
+            end
+        elseif i == 2
+            for x=sideranges[3], z=sideranges[4]
+                xpos = x + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                zpos = z + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(xpos, 0, zpos)
+                neuronid += 1
+            end
+        elseif i == 3
+            for y=sideranges[5], z=sideranges[6]
+                ypos = y + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                zpos = z + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(0, ypos, zpos)
+                neuronid += 1
+            end
+        elseif i == 4
+            for x=sideranges[7], y=sideranges[8]
+                xpos = x + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                ypos = y + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(xpos, ypos, lastside)
+                neuronid += 1
+            end
+        elseif i == 5
+            for x=sideranges[9], z=sideranges[10]
+                xpos = x + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                zpos = z + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(xpos, lastside, zpos)
+                neuronid += 1
+            end
+        elseif i == 6
+            for y=sideranges[11], z=sideranges[12]
+                neuronid > neuronsnumber && break
+
+                ypos = y + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                zpos = z + sideoffset + rand(Uniform(-1, 1)) * randomscale
+                state.neuronpositions[neuronid] = Point3f(lastside, ypos, zpos)
+                neuronid += 1
+            end
+        end
+    end
+
+    neuronpositions = collect(values(state.neuronpositions))
+    neuroncounter = data[collect(diagind(data))]
+    neuroncounter_scaled = minmax(neuroncounter)
+    
+    innercolors = repeat([neuron_innercolor], length(neuronpositions))
+    innersizes = repeat([neuron_innersize], length(neuronpositions)) .+ 
+        neuroncounter_scaled .* 0.15
+    meshscatter!(
+        parentscene, neuronpositions, 
+        markersize=innersizes, color=innercolors
+    )
+    
+    outercolors = repeat([neuron_outercolor], length(neuronpositions))
+    outersizes = repeat([neuron_outersize], length(neuronpositions)) .+ 
+        neuroncounter_scaled .* 0.15
+    meshscatter!(
+        parentscene, neuronpositions, 
+        markersize=outersizes, color=outercolors
+    )
 
     figure
 end
 
-function createscenes(resolution, camera3d)
+function createscenes(resolution, camera3d, background)
     figure = Figure()
     rowsize!(figure.layout, 1, Fixed(resolution[2]))
 
-    # pl = PointLight(Point3f(0, 0, 15), RGBf(1.0, 0.98, 0.94))
-    al = AmbientLight(RGBf(0.58, 0.58, 0.58))
+    al = AmbientLight(RGBf(1.0, 1.0, 1.0))
 
     parentscene = LScene(
         figure[:, 1],
@@ -51,10 +179,7 @@ function createscenes(resolution, camera3d)
         scenekw = (
             clear=true,
             lights=[al],
-            backgroundcolor=:black,
-            # ssao = Makie.SSAO(radius=250.0, blur=2, bias=1),
-            # lightposition = Vec3f(0, 0, 15),
-            shininess=256f0
+            backgroundcolor=background
         )
     )
 
@@ -75,4 +200,11 @@ function primary_resolution()
     monitor = GLMakie.GLFW.GetPrimaryMonitor()
     videomode = GLMakie.MonitorProperties(monitor).videomode
     return (videomode.width, videomode.height)
+end
+
+function minmax(array::AbstractArray)
+    min = minimum(array)
+    max = maximum(array)
+    range = max - min
+    (array .- min) ./ range
 end
